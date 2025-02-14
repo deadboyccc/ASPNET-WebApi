7f49f946a567ea9a25e109937bd1f6308ea67eca
Ahmed Ali <ahmed.mohammed.ali9999@gmail.com>
2025-02-14 11:21:54 +0300

    GetById implemented, started with the DTOs

--- BGwalks/BGwalks.API/Controllers/RegionsController.cs
+++ BGwalks/BGwalks.API/Controllers/RegionsController.cs
@@ -1,3 +1,4 @@
+using BGwalks.API.Data;
 using BGwalks.API.Models.Domain;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -9,21 +10,46 @@ namespace BGwalks.API.Controllers
     [ApiController]
     public class RegionsController : ControllerBase
     {
+        // Dependency injection when the controller is created inject the db context that is injected/coming from the asp app
+        private readonly BGWalksDbContext _dbContext;
+        public RegionsController(BGWalksDbContext dbContext)
+        {
+            this._dbContext = dbContext;
+        }
         [HttpGet]
         public IActionResult GetAll()
         {
-            // hard code
-            var regions = new List<Region>()
-            {
-                new Region(){Id=Guid.NewGuid(),Name = "Mansoor"},
-                new Region(){Id=Guid.NewGuid(),Name = "Karada"},
-                new Region(){Id=Guid.NewGuid(),Name = "New York"}
-                
-                
-            };
+            var regions = _dbContext.Regions.ToList();
+
             // 200
             return Ok(regions);
         }
-        
+
+
+        // get by id (:guid) == typesafety
+        [HttpGet("{id:guid}")]
+        public IActionResult GetById([FromRoute] Guid id)
+        {
+            // get data from the domain models
+            var region = _dbContext.Regions.Find(id);
+
+            // DTO implementation  - map domain models to DTOs
+
+
+            // linq
+            // var region = _dbContext.Regions.FirstOrDefault(r => r.Id == id);
+
+            // 200 if found, 404 if not found
+            if (region == null)
+            {
+                // 404 
+                return NotFound();
+            }
+            // return DTO
+            return Ok(region);
+
+        }
+
     }
+
 }
