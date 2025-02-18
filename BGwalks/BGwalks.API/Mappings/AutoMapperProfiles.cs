aa93837174a31e598659c1aa40e896c18105fd60
Ahmed Ali <ahmed.mohammed.ali9999@gmail.com>
2025-02-18 16:15:57 +0300

    bugfix: added the necessary automapper walk implementation

--- BGwalks/BGwalks.API/Mappings/AutoMapperProfiles.cs
+++ BGwalks/BGwalks.API/Mappings/AutoMapperProfiles.cs
@@ -8,10 +8,15 @@ class AutoMapperProfiles : Profile
   public AutoMapperProfiles()
   {
     // CreateMap <source, destination>() 
+
+    // region resource
     CreateMap<RegionDomain, RegionGetDto>().ReverseMap();
     CreateMap<RegionDomain, regionCreateDto>().ReverseMap();
     CreateMap<RegionDomain, RegionUpdateDto>().ReverseMap();
-    CreateMap<WalkCreateDto, WalkDomain>().ReverseMap();
+
+    // walk resource
+    CreateMap<WalkDomain, WalkCreateDto>().ReverseMap();
+    CreateMap<WalkDomain, WalkGetDto>().ReverseMap();
 
   }
 
